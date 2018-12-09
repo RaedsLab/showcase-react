c6dc75577f39cb19f340c1cbf01056f3d56aaade
Raedslab <raed.chammam@gmail.com>
2018-12-09 12:12:21 +0100

    add some more tests

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -4,23 +4,21 @@ export const GET_PLANETS_REQUEST = 'planets/GET_PLANETS';
 export const GET_PLANETS_SUCCESS = 'planets/GET_PLANETS_SUCCESS';
 export const GET_PLANETS_ERROR = 'planets/GET_PLANETS_ERROR';
 
-export function getPlanets() {
-  return dispatch => {
-    dispatch({
-      type: GET_PLANETS_REQUEST
-    });
-    fecthNearEarthObjects()
-      .then(response => {
-        dispatch({
-          type: GET_PLANETS_SUCCESS,
-          data: response
-        });
-      })
-      .catch(error => {
-        dispatch({
-          type: GET_PLANETS_ERROR,
-          error
-        });
+export const getPlanets = () => dispatch => {
+  dispatch({
+    type: GET_PLANETS_REQUEST
+  });
+  return fecthNearEarthObjects()
+    .then(response => {
+      dispatch({
+        type: GET_PLANETS_SUCCESS,
+        data: response
       });
-  };
-}
+    })
+    .catch(error => {
+      dispatch({
+        type: GET_PLANETS_ERROR,
+        error
+      });
+    });
+};
