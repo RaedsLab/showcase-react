573272a27b61df286e7a22d1ea09815174a57e59
Raedslab <raed.chammam@gmail.com>
2018-12-10 21:00:55 +0100

    use composeWithDevTools instead of global instance

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './reducers/root';
 
@@ -18,17 +19,20 @@ const initialState = {
 const enhancers = [];
 const middleware = [thunk];
 
+let composedEnhancers;
+
 if (process.env.NODE_ENV === 'development') {
-  const devToolsExtension = window['__REDUX_DEVTOOLS_EXTENSION__'];
-  if (typeof devToolsExtension === 'function') {
-    enhancers.push(devToolsExtension());
-  }
+  composedEnhancers = composeWithDevTools(
+    applyMiddleware(...middleware),
+    ...enhancers
+  );
+} else {
+  composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+  );
 }
 
-const composedEnhancers = compose(
-  applyMiddleware(...middleware),
-  ...enhancers
-);
 const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
