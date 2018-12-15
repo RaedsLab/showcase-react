33a5bb2cafa05f6fc7f86e4f5e2a67637eb82d08
Raedslab <raed.chammam@gmail.com>
2018-12-15 12:37:39 +0100

    fix react router test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { createMount } from '@material-ui/core/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+import { shape } from 'prop-types';
+
 import Header from './Header';
 
 describe('<Header />', () => {
+  const router = {
+    history: new BrowserRouter().history,
+    route: {
+      location: {},
+      match: {}
+    }
+  };
+  const createContext = () => ({
+    context: { router },
+    childContextTypes: { router: shape({}) }
+  });
+
   it('should not crash', () => {
     const mount = createMount();
     const props = {
@@ -11,7 +26,7 @@ describe('<Header />', () => {
     };
 
     //const wrapper = shallow(<Header {...props} />);
-    const component = mount(<Header {...props} />);
+    const component = mount(<Header {...props} />, createContext());
 
     component
       .find('input')
