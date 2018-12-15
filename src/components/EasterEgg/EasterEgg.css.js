dfd84735d144c0ffa51ada14155b864b05990a74
Raedslab <raed.chammam@gmail.com>
2018-12-15 17:04:29 +0100

    BREAKS TESTS ! added easter egg konami hook

---
+++ src/components/EasterEgg/EasterEgg.css.js
@@ -0,0 +1,12 @@
+const styles = theme => ({
+  paper: {
+    padding: theme.spacing.unit * 2,
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    flexShrink: 1,
+    flexGrow: 1
+  }
+});
+
+export default styles;
---
+++ src/service/service.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+export async function fecthNearEarthObjects(): Promise<object> {
+  try {
+    const data = await get(
+      'https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY'
+    );
+    return data;
+  } catch (error) {
+    /**
+     * 429 TOO MANY REQUESTS
+     * https://httpstatuses.com/429
+     * If NASA server is not responding, use local version of data
+     *  */
+    if (error.response && error.response.status === 429) {
+      return get('/nasa.json');
+    }
+    return Promise.reject(error);
+  }
+}
+
+export function fetchPlanetInfo(planet: string): Promise<object> {
+  // cross-anywhere is used to allow crossdimain (sacrifising performance)
+  return get(
+    `https://cors-anywhere.herokuapp.com/en.wikipedia.org/w/api.php?action=opensearch&limit=1&format=json&search=${planet}`
+  );
+}
+
+async function get(URL: string): Promise<object> {
+  const reposne = await axios.get(URL);
+  return reposne.data;
+}
